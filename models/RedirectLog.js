59f0a7fce30f301af8cd38db8290b79d3bec5363
pankajvitaldeveloper <pankajkumardeveloper0@gmail.com>
2025-08-03 23:46:30 +0530

    change in model redirect and transferRoutes

--- models/RedirectLog.js
+++ models/RedirectLog.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const redirectLogSchema = new mongoose.Schema({
   uid: String,
   pid: String,
+  projectName: String, // Add this line
   status: { type: String, enum: ['completed', 'terminate', 'quotafull'] },
   ip: String,
   userAgent: String,
-  location: Object,     // geoIP API response
-  fingerprint: String,  // from client JS
+  location: Object,
+  fingerprint: String,
   createdAt: { type: Date, default: Date.now }
 });
 
