547dee4f094fb1f9ce1e863fdcb62e336abcc4ed
pankajvitaldeveloper <pankajkumardeveloper0@gmail.com>
2025-08-03 21:15:39 +0530

    url c

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 
 // Load env
-dotenv.config();
+dotenv.config({ quiet: true });
 
 // DB Connect
 connectDB();
@@ -12,7 +12,7 @@ connectDB();
 // Init app
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json());    
 
 const transferRoutes = require('./routes/transferRoutes');
 
